434b412d7eea1b0305ea5576451656f56fba46e9
Abdul Wahab Khan <abdulwahabchughtai99@gmail.com>
2023-11-26 23:43:20 -0500

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'wahab',
+    address: 'maryland'
+};
+
+function updateEmployeeWithKeyAndValue(object,key,value){
+    const newObject = {...object}
+    newObject[key] = value
+    return newObject
+}
+updateEmployeeWithKeyAndValue(employee,'name','khan')
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+    const newObject = object
+    newObject[key] = value
+    return newObject
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee,'name','khan')
+
+function deleteFromEmployeeByKey(employee, key){
+    const newObject = {...employee}
+    delete newObject[key]
+    return newObject
+}
+deleteFromEmployeeByKey(employee,'name')
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newObject = employee
+    delete newObject[key]
+    return newObject
+}
+deleteFromEmployeeByKey(employee,'name')
